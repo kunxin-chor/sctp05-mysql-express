7e5799377180d4e56b5aedc96652f1054e71e87e
kunxin-chor <chorkunxin@yahoo.com>
2024-12-20 03:31:10 +0000

    Added Create Customer

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const app = express();
 // <-- inform Express that we are using hbs as our view engine
 app.set('view engine', 'hbs');
 
+// setup form processing for Express
+// VERY IMPORTANT!
+app.use(express.urlencoded({
+    extended: false // set to false for fast form processing but without advanced features
+}))
+
 async function main() {
     // create a connection to our MySQL database
     const connection = await createConnection({
@@ -29,6 +35,7 @@ async function main() {
         // SQL to execute: SELECT * FROM Customers
         const query = `SELECT * FROM Customers JOIN 
                         Companies ON Companies.company_id = Customers.company_id
+                        ORDER BY Customers.customer_id DESC
                     `;
     
         // INSTEAD OF:
@@ -44,13 +51,48 @@ async function main() {
             'nestTables': true
         });
 
-        console.log(customers);
+    
 
         res.render('customers.hbs', {
             "allCustomers": customers
         })
     })
 
+    // In a dynmaic web app, for form processing, there are always 2 routes
+    // 1st route -> display the form (GET)
+    // 2nd route -> process the form (POST)
+
+    // display the form
+    app.get('/customers/create', async function(req,res){
+        const [companies] = await connection.execute("SELECT * FROM Companies")
+        console.log(companies);
+        res.render('create_customer',{
+            'companies': companies
+        });
+    })
+    
+    // process the form
+    app.post('/customers/create', async function(req,res){
+        //  const first_name = req.body.first_name;
+        //  const last_name = req.body.last_name;
+        //  const rating = req.body.rating;
+        //  const company_id = req.body.company_id;
+        // Instead we can use object structuring
+        const { first_name, last_name, rating, company_id} = req.body;
+
+        // WARNING: DON'T USE THE CODE BELOW, IT'S SUSPECTIBLE TO SQL INJECTION ATTACKS
+        // const query = `INSERT INTO Customers (first_name, last_name, rating, company_id) VALUES ("${first_name}", 
+        //             "${last_name}", ${rating}, ${company_id})`;
+        // console.log(query);
+        // const results = await connection.execute(query);
+
+        // INSTEAD, we'll use PREPARED STATEMENTS
+        const query = "INSERT INTO Customers (first_name, last_name, rating, company_id) VALUES (?, ?, ?, ?);"
+        const results = await connection.execute(query, [first_name, last_name, rating, company_id]);
+
+        // tell the front end to go this particular route
+        res.redirect('/customers');
+    })
    
 }
 main();
